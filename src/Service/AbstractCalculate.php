d7cd5b26cd3188d038475244f32e21646433aaf5
Manol Trendafilov <m.trendafilov@propy.com>
2020-03-11 16:33:16 +0200

    implement the logic for the calculations

--- src/Service/AbstractCalculate.php
+++ src/Service/AbstractCalculate.php
@@ -3,19 +3,119 @@
 
 namespace App\Service;
 
+use App\Data\ItemDTO;
+
 abstract class AbstractCalculate implements CalculateInterface
 {
-    protected $queue;
-    protected $products;
+    protected $productNames = [];
+
+    /**
+     * @var ItemDTO[] $fixtureItems
+     */
+    protected $fixtureItems = [];
+    /**
+     * @var string $products
+     * @desc this is the incoming string ZA,YB,FC,GD,ZA,YB,ZA,ZA divided by commas
+     */
+    protected string $products;
+
+    /**
+     * @var array $scannedItems
+     * @desc items after they were scanned on the terminal line
+     */
+    protected $scannedItems = [];
+
+    protected $totalPrice;
+
     function __construct()
     {
-        $this->queue = new \SplQueue();
     }
 
     function setProducts($products)
     {
         $this->products = $products;
+        if(strpos($this->products, ',') !== false) {
+            $this->productNames = explode(",",$this->products);
+        } else {
+            $this->productNames[] = $products;
+        }
     }
+
+    protected function isValidProduct($scannedItem)
+    {
+        /** @var ItemDTO $item */
+        foreach($this->fixtureItems as $item) {
+            if($item->getName() == $scannedItem && isset($this->fixtureItems[$scannedItem])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function setFixtureItems(array $fixtureItems)
+    {
+        $this->fixtureItems = $fixtureItems;
+    }
+
+    function process()
+    {
+        foreach($this->productNames as $productName) {
+            $this->scanItem($productName);
+        }
+
+        if(count($this->scannedItems) == 0) {
+            return;
+        }
+
+        $occurence = $this->findOccurrence($this->products);
+        $totalPrice = 0;
+
+        foreach($this->scannedItems as $scannedItem) {
+            /** @var ItemDTO $item */
+            $item = $this->fixtureItems[$scannedItem];
+            if(!$item->getPromotionPrice() && !$item->getPromotionQty()) {
+                // the item does not have a promotion
+                $price = $occurence[$scannedItem] * $item->getPrice();
+                $totalPrice += $price;
+                continue;
+            }
+
+            $price = 0;
+            // calc the number of the promotions
+            $promotionsNum = floor($occurence[$scannedItem]/$item->getPromotionQty());
+            if($promotionsNum > 0) {
+                for($i = 0; $i < $promotionsNum; $i++) {
+                    $price += $item->getPromotionPrice();
+                    $occurence[$scannedItem] -= $item->getPromotionQty();
+                }
+            }
+
+            if($occurence[$scannedItem] > 0) {
+                $price += $occurence[$scannedItem] * $item->getPrice();
+            }
+
+            $totalPrice += $price;
+        }
+
+        $this->totalPrice = $totalPrice;
+    }
+
+    public function findOccurrence() {
+        $occurence = [];
+
+        foreach($this->productNames as $productName) {
+
+            if(isset($occurence[$productName])) {
+                $occurence[$productName] = 0;
+            } else {
+                $occurence[$productName] += 1;
+            }
+        }
+
+        return $occurence;
+    }
+
 
     abstract function getTotal();
     abstract function scanItem($item);
