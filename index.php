ab862c2ee0f6c2ca5eac15d2647d97d45b7f74da
Manol Trendafilov <m.trendafilov@propy.com>
2020-03-11 16:31:02 +0200

    inject fixtures

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require 'vendor/autoload.php';
 
 use App\ConsoleApp;
 use App\Service\Calculate;
+use App\Service\ItemFixtures;
 
-$app = new ConsoleApp(new Calculate());
+$app = new ConsoleApp(new Calculate(), new ItemFixtures());
 $app->run($argv);
